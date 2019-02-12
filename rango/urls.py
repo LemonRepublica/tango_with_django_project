91034690059b995b856f28f026501d562e2d29a8
Wenjun Li <2381366l@student.gla.ac.uk>
2019-02-12 00:12:20 +0000

    ch4 done

--- rango/urls.py
+++ rango/urls.py
@@ -1,7 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  5 17:16:15 2019
+
+@author: 10536
+"""
+
 from django.conf.urls import url
 from rango import views
-urlpatterns= [
-    url(r'^$',views.index, name='index'),
-    url(r'^about/',views.about, name='about'),
-    
-]
+
+
+
+urlpatterns = [
+        url(r'^$',views.index,name='index'),
+        url(r'^about/',views.about,name='about'),
+] 
